860bb1f797a6ffecc3ae893ecfb03a0e943ecd82
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-09-05 19:17:09 +0000

    commserver.ua.server - remove direct references - Create UA Configuration Library package  - updated CommServer.UA.ConfigurationEditor  - prepared for release: 3.20.1-Hotel  - unit tested. Issue #: 4863

---
+++ VS/trunk/CommServer.UA.ConfigurationEditor/UnitTests/ConfigurationEditor.ServerConfiguration.UnitTests/ResourcesManagementUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using CAS.CommServer.UA.ConfigurationEditor.ServerConfiguration.EmbeddedResources;
+
+namespace CAS.CommServer.UA.Server.ServerConfiguration.UnitTests
+{
+  [TestClass]
+  public class ResourcesManagementUnitTest
+  {
+    [TestMethod]
+    public void GetDefaultServerConfigurationFileMethod()
+    {
+      Stream _defaultConfigurationFile = ResourcesManagement.GetDefaultServerConfigurationFile();
+      Assert.IsNotNull(_defaultConfigurationFile);
+    }
+  }
+}
