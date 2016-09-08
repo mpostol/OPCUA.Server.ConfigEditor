b71dca5a4629177b400e0ab804acb6ba38e37653
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-09-08 09:53:42 +0000

    commserver.ua.server - remove direct references - ApplicationCertificate unit tests  - added unit tests to CommServer.UA.ConfigurationEditor Issue #: 4863

---
+++ VS/trunk/CommServer.UA.ConfigurationEditor/UnitTests/ConfigurationEditor.ServerConfiguration.UnitTests/CASConfigurationUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAS.UA.Server.ServerConfiguration;
+
+namespace CAS.CommServer.UA.Server.ServerConfiguration.UnitTests
+{
+  [TestClass]
+  public class CASConfigurationUnitTest
+  {
+    [TestMethod]
+    public void ConstructorTestMethod()
+    {
+      CASConfiguration _newConfiguration = new CASConfiguration();
+      Assert.IsNull(_newConfiguration.CASExtension);
+    }
+  }
+}
