7f7a54d38bb26e94c2dd863b68b40a5c595338b3
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-09-08 14:41:06 +0000

    commserver.ua.server - remove direct references - ApplicationCertificate unit tests  - tested against Main singleton behavior. Issue #: 4863

---
+++ VS/trunk/CommServer.UA.ConfigurationEditor/UnitTests/ConfigurationEditor.ServerConfiguration.UnitTests/AssemblyInitializeClass.cs
@@ -0,0 +1,36 @@
+﻿
+using CAS.UA.Server.ServerConfiguration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace CAS.CommServer.UA.Server.ServerConfiguration.UnitTests
+{
+  [TestClass]
+  [DeploymentItem(m_Path, m_Path)]
+  public class AssemblyInitializeClass
+  {
+    internal static string FilePath { get { return Path.Combine(m_Path, m_ConfigurationFileName); } }
+    internal const string m_Path = @"EmbeddedExample\DemoConfiguration";
+    internal const string m_ConfigurationFileName = @"BoilerExample.uasconfig";
+    private static TestContext m_Context;
+
+    [AssemblyInitialize()]
+    public static void ClassInitializeMethod(TestContext context)
+    {
+      m_Context = context;
+    }
+    [ClassCleanup]
+    public static void ClassCleanupMethod()
+    {
+      if (Main.EntryPoint == null)
+        throw new System.ApplicationException($"Program error {nameof(Main.EntryPoint)} is empty.");
+    }
+    [TestMethod]
+    public void DeploymentTest()
+    {
+      FileInfo file = new FileInfo(AssemblyInitializeClass.FilePath);
+      Assert.IsTrue(file.Exists);
+      Assert.IsNotNull(m_Context);
+    }
+  }
+}
--- VS/trunk/CommServer.UA.ConfigurationEditor/UnitTests/ConfigurationEditor.ServerConfiguration.UnitTests/ResourcesManagementUnitTest.cs
+++ VS/trunk/CommServer.UA.ConfigurationEditor/UnitTests/ConfigurationEditor.ServerConfiguration.UnitTests/ResourcesManagementUnitTest.cs
@@ -1,40 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
+﻿
 using CAS.CommServer.UA.ConfigurationEditor.ServerConfiguration.EmbeddedResources;
-using CAS.UA.Server.ServerConfiguration;
-using Opc.Ua;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace CAS.CommServer.UA.Server.ServerConfiguration.UnitTests
 {
   [TestClass]
-  [DeploymentItem(m_Path, m_Path)]
   public class ResourcesManagementUnitTest
   {
     [TestMethod]
     public void GetDefaultServerConfigurationFileMethod()
     {
-      Stream _defaultConfigurationFile = ResourcesManagement.GetDefaultServerConfigurationFile();
-      Assert.IsNotNull(_defaultConfigurationFile);
-    }
-    [TestMethod]
-    public void DeploymentTest()
-    {
-      FileInfo file = new FileInfo(FilePath);
-      Assert.IsTrue(file.Exists);
-    }
-    [TestMethod]
-    public void LoadTestMethod()
-    {
-      using (Main _MAIN = new Main())
+      using (Stream _defaultConfigurationFile = ResourcesManagement.GetDefaultServerConfigurationFile())
       {
-        FileInfo _FileInfo = new FileInfo(FilePath);
-        CASConfiguration _configuration = CASConfiguration.Load(_FileInfo, ApplicationType.Server, typeof(CASConfiguration), false);
-        Assert.IsNotNull(_configuration);
+        Assert.IsNotNull(_defaultConfigurationFile);
       }
     }
-    private string FilePath { get { return Path.Combine(m_Path, m_ConfigurationFileName); } }
-    private const string m_Path = @"EmbeddedExample\DemoConfiguration";
-    private const string m_ConfigurationFileName = @"BoilerExample.uasconfig";
-
   }
 }
