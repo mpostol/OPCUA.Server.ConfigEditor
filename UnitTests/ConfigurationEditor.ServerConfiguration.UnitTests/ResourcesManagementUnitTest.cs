a9326d3016f108190062fcb06fcd332edb7ed498
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-09-08 09:53:42 +0000

    commserver.ua.server - remove direct references - ApplicationCertificate unit tests  - added unit tests to CommServer.UA.ConfigurationEditor Issue #: 4863

--- UnitTests/ConfigurationEditor.ServerConfiguration.UnitTests/ResourcesManagementUnitTest.cs
+++ UnitTests/ConfigurationEditor.ServerConfiguration.UnitTests/ResourcesManagementUnitTest.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using CAS.CommServer.UA.ConfigurationEditor.ServerConfiguration.EmbeddedResources;
+using CAS.UA.Server.ServerConfiguration;
+using Opc.Ua;
 
 namespace CAS.CommServer.UA.Server.ServerConfiguration.UnitTests
 {
   [TestClass]
+  [DeploymentItem(m_Path, m_Path)]
   public class ResourcesManagementUnitTest
   {
     [TestMethod]
@@ -13,5 +16,25 @@ namespace CAS.CommServer.UA.Server.ServerConfiguration.UnitTests
       Stream _defaultConfigurationFile = ResourcesManagement.GetDefaultServerConfigurationFile();
       Assert.IsNotNull(_defaultConfigurationFile);
     }
+    [TestMethod]
+    public void DeploymentTest()
+    {
+      FileInfo file = new FileInfo(FilePath);
+      Assert.IsTrue(file.Exists);
+    }
+    [TestMethod]
+    public void LoadTestMethod()
+    {
+      using (Main _MAIN = new Main())
+      {
+        FileInfo _FileInfo = new FileInfo(FilePath);
+        CASConfiguration _configuration = CASConfiguration.Load(_FileInfo, ApplicationType.Server, typeof(CASConfiguration), false);
+        Assert.IsNotNull(_configuration);
+      }
+    }
+    private string FilePath { get { return Path.Combine(m_Path, m_ConfigurationFileName); } }
+    private const string m_Path = @"EmbeddedExample\DemoConfiguration";
+    private const string m_ConfigurationFileName = @"BoilerExample.uasconfig";
+
   }
 }
