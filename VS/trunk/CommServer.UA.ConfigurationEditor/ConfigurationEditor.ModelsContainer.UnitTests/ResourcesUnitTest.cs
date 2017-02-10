ff63ec9204c606582294bf50ccef2d665accf7ad
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2017-02-10 10:54:44 +0000

    Change CAS.UA.IServerConfiguration add-in installation directory - Removed dependency on the ConfigurationEditor.ServerConfiguration add-in. - Improved UT to count number of files installed as the embedded example
    Issue #: 4779

--- VS/trunk/CommServer.UA.ConfigurationEditor/ConfigurationEditor.ModelsContainer.UnitTests/ResourcesUnitTest.cs
+++ VS/trunk/CommServer.UA.ConfigurationEditor/ConfigurationEditor.ModelsContainer.UnitTests/ResourcesUnitTest.cs
@@ -16,11 +16,13 @@ namespace CAS.CommServer.UA.ConfigurationEditor.ModelsContainer.UnitTests
       {
         string _path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), m_TestPath);
         _targetDirectory = new DirectoryInfo(_path);
+        int _installedFiles = 0;
         Assert.IsFalse(_targetDirectory.Exists, "The test directory already exist before test starting");
-        ContainerResources.ExampleSolutionInstallation(m_TestPath, (x, y) => Console.WriteLine($"{y}: {x}"));
+        ContainerResources.ExampleSolutionInstallation(m_TestPath, (x, y) => { Console.WriteLine($"{y}: {x}"); _installedFiles++; });
+        Assert.AreEqual<int>(20, _installedFiles);
         _targetDirectory.Refresh();
         Assert.IsTrue(_targetDirectory.Exists);
-        Assert.AreEqual<int>(3, _targetDirectory.GetFiles().Length, "Expected 2 solutions and plug-in");
+        Assert.AreEqual<int>(2, _targetDirectory.GetFiles().Length, "Expected 2 solutions and plug-in");
         Assert.AreEqual<int>(2, _targetDirectory.GetDirectories().Length, "Expected only 2 solutions folders");
       }
       catch (Exception)
